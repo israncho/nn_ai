d410a69df73b7bf731380a14ecf60bfeff95be13
Jesus Israel Gutierrez Elizalde <isragutierrez@ciencias.unam.mx>
2024-09-06 21:28:46 -0600

    binary entropy node

--- src/nodes.py
+++ src/nodes.py
@@ -38,11 +38,11 @@ class OperationNode(ABC):
     '''Clase abstracta para nodos que realizan calculos
     con datos de entrada y salidas de otros nodos.'''
 
-    def __call__(self, x) -> Union[np.ndarray, float]:
-        return self.forward(x)
+    def __call__(self, *args) -> Union[np.ndarray, float]:
+        return self.forward(*args)
 
     @abstractmethod
-    def forward(self, x) -> Union[np.ndarray, float]:
+    def forward(self, *args) -> Union[np.ndarray, float]:
         '''Ejecuta la operación con la entrada proporcionada.'''
 
 
@@ -54,7 +54,8 @@ class PreActivation(OperationNode):
         self.w = w
         self.b = b
 
-    def forward(self, x):
+    def forward(self, *args):
+        x = args[0]
         return np.dot(self.w(), x.T) + self.b()
 
 
@@ -65,5 +66,36 @@ class Sigmoid(OperationNode):
     def __init__(self, previous: OperationNode):
         self.previous = previous
 
-    def forward(self, x):
+    def forward(self, *args):
+        x = args[0]
         return 1 / (1 + np.exp(- self.previous(x)))
+
+
+class BinCrossEntropy(OperationNode):
+    '''Nodo que calcula la entropía cruzada binaria entre las
+    predicciones del modelo y las etiquetas verdaderas.'''
+
+    def __init__(self, previous: OperationNode):
+        self.previous = previous
+
+    def forward(self, *args):
+        x = args[0]
+        y = args[1]
+        f_x = self.previous(x)
+        return -(y * np.log(f_x) + (1 - y) * np.log(1 - f_x))
+
+
+if __name__ == "__main__":
+    logistic_reg = Sigmoid(PreActivation(Weights(2), Bias()))
+    data_set = np.array([[1, 1], [2, 2], [3, 3], [2, 3]])
+    data_set_labels = np.array([1, 1, 0, 0])
+    print("dataset:\n", data_set)
+    print("etiquetas:", data_set_labels)
+    # regresion logistica para cada entrada
+    # del dataset
+    print("regresion del dataset: ", logistic_reg(data_set))
+    # regresion logistica de un solo dato del dataset
+    print("regresion de un solo dato: ", logistic_reg(data_set[0]))
+    loss = BinCrossEntropy(logistic_reg)
+    # error de cada entrada del dataset
+    print("error: ", loss(data_set, data_set_labels))
