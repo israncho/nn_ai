4f7f339cc08ac7268bdceaea9b91dcdd318b924a
Jesus Israel Gutierrez Elizalde <isragutierrez@ciencias.unam.mx>
2024-10-15 18:05:49 -0600

    probando batch y estocastico

--- src/test.py
+++ src/test.py
@@ -13,14 +13,19 @@ if __name__ == "__main__":
     x_train, x_eval, y_train, y_eval = train_test_split(x, y, test_size=0.3)
 
 
+    print('Stochastic gradient descent:')
     preact = PreActivation(2)
     sigm = Sigmoid(preact)
     loss = BinCrossEntropy(sigm)
     loss_history = []
     lr = 0.1
+
     for _ in range(100):
-        sigm(preact(x))
-        loss_history.append(np.mean(loss(y)))
+        sigm(preact(x_eval))
+        eval_loss = np.mean(loss.forward(y_eval))
+        sigm(preact(x_train))
+        train_loss = np.mean(loss.forward(y_train))
+        loss_history.append((train_loss, eval_loss))
         for x_i, y_i in zip(x_train, y_train):
             sigm(preact(x_i)) # forward
             loss.backward(y_i) # backward
@@ -28,8 +33,44 @@ if __name__ == "__main__":
             preact.w -= lr * grad_w
             preact.b -= lr * grad_b
 
-    for i, loss in enumerate(loss_history[:10]):
-        print("epoca:",i ,"\terror:", loss)
+    for i, (t_loss, ev_loss) in enumerate(loss_history[::10]):
+        print("epoch:", i*10,"\ttrain_loss:", t_loss, 'ev_loss:', ev_loss)
+
+    print("\n")
+    y_prob = sigm(preact(x_eval))
+    y_pred = (y_prob > 0.5).astype(int)
+    print(classification_report(y_eval, y_pred))
+
+    #-----------------------------------------------------------------
+
+    print('\n\nBatch gradient descent:')
+
+    preact = PreActivation(2)
+    sigm = Sigmoid(preact)
+    loss = BinCrossEntropy(sigm)
+    loss_history = []
+    lr = 0.1
+    for _ in range(100):
+        sigm(preact(x_eval))
+        eval_loss = np.mean(loss.forward(y_eval))
+        sigm(preact(x_train))
+        train_loss = np.mean(loss.forward(y_train))
+        loss_history.append((train_loss, eval_loss))
+
+        sigm(preact(x_train))
+        loss.backward(y_train)
+
+        grads_w, grads_b = preact.grad
+
+        grads_w = np.sum(grads_w, axis=0)
+
+        grads_b = np.sum(grads_b, axis=0)
+
+        preact.w -= lr * grad_w
+        preact.b -= lr * grad_b
+
+    for i, (t_loss, ev_loss) in enumerate(loss_history[::10]):
+        print("epoch:", i*10,"\ttrain_loss:", t_loss, 'ev_loss:', ev_loss)
 
     print("\n")
     y_prob = sigm(preact(x_eval))
